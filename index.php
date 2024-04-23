521eaf3d07bf5029fa59af28871dec0bb7b23259
Edimar Ferla de Almeida <efa.ferla@gmail.com>
2024-04-23 09:36:46 -0300

    Mudando o Projeto para PHP
    Estou criando uma atualização para o projeto ser em PHP.

---
+++ index.php
@@ -0,0 +1,112 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br" class="no-js">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <title>Edimar Ferla</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="Curriculum Vitae Edimar Ferla" />
+    <meta name="keywords" content="curriculo, vitae, git, github, Ferla, Edimar, Ferla Interativa, Marketing Digital" />
+    <link rel="shortcut icon" href="/images/icon/favicon.ico">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/normalize.css" type="text/css">
+    <link rel="stylesheet" href="css/animate.css" type="text/css">
+    <link rel="stylesheet" href="css/transition-animations.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/main-green.css" type="text/css">
+    <link rel="stylesheet" href="css/progress.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script src="js/modernizr.custom.js"></script>
+</head>
+
+<body class="material-template">
+    <!-- Loading animation -->
+    <div id="loadingScreen">
+      <div id="progressBar">
+          <div></div>
+      </div>
+  </div>
+    <!-- /Loading animation -->
+
+    <div id="page" class="page">
+      <!-- Header -->
+      <header id="site_header" class="header mobile-menu-hide">
+        <div class="header-content">
+          <div class="site-title-block mobile-hidden">
+            <a href="index.php#home"><div class="site-title">Edimar <span>Ferla</span></div></a>
+          </div>
+
+          <!-- Navigation -->
+          <div class="site-nav">
+            <!-- Main menu -->
+            <ul id="nav" class="site-main-menu">
+              <li>
+                <a class="pt-trigger" href="index.php#home">Início</a>
+              </li>
+              <li>
+                <a class="pt-trigger" href="index.php#resume">Currículo</a>
+              </li>
+              <li>
+                <a class="pt-trigger" href="index.php#services">Serviços</a>
+              </li>
+              <li>
+                <a class="pt-trigger" href="index.php#portfolio">Portfólio</a>
+              </li>
+            </ul>
+            <!-- /Main menu -->
+          </div>
+          <!-- Navigation -->
+        </div>
+      </header>
+      <!-- /Header -->
+
+      <!-- Mobile Header -->
+      <div class="mobile-header mobile-visible">
+        <div class="mobile-logo-container">
+          <div class="mobile-site-title">Edimar Ferla</div>
+        </div>
+
+        <a class="menu-toggle mobile-visible">
+          <i class="fa fa-bars"></i>
+        </a>
+      </div>
+      <!-- /Mobile Header -->
+
+      <!-- Main Content -->
+      <div id="main" class="site-main">
+        <!-- Page changer wrapper -->
+        <div class="pt-wrapper">
+          <!-- Subpages -->
+          <div class="subpages">
+            <?php
+            // Exemplo de inserção de PHP
+            echo "<p>Este é um exemplo de conteúdo dinâmico em PHP!</p>";
+            ?>
+            <!-- Inserir mais código PHP conforme necessário -->
+          </div>
+          <!-- /Subpages -->
+        </div>
+        <!-- /Page changer wrapper -->
+      </div>
+      <!-- /Main Content -->
+    </div>
+    <footer>
+      <div class="copyrights">&copy; Edimar Ferla</div>
+    </footer>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/pages-switcher.js"></script>
+    <script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
+    <script type="text/javascript" src="js/validator.js"></script>
+    <script type="text/javascript" src="js/jquery.shuffle.min.js"></script>
+    <script type="text/javascript" src="js/masonry.pkgd.min.js"></script>
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="js/jquery.hoverdir.js"></script>
+    <script type="text/javascript" src="js/jquery.googlemap.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    <script src="js/progress.js"></script>
+  </body>
+</html>
