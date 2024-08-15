19f39411603b09ff30a22791420b8ec592c1e251
INDRA\eferla <eferla@minsait.com>
2024-08-15 17:18:03 -0300

    revert update java

--- js/progress.js
+++ js/progress.js
@@ -1,24 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var progressValue = document.getElementById("progressValue");
-    var loadingScreen = document.getElementById("loadingScreen");
-    var progress = 0;
+document.addEventListener("DOMContentLoaded", function() {
+  var progressBar = document.getElementById("progressBar").firstElementChild;
+  
+  function updateProgressBar(percentage) {
+      progressBar.style.width = percentage + '%';
+  }
 
-    function simulateLoading() {
-        if (progress < 100) {
-            progress += Math.random() * 10; // Simula um carregamento irregular
-            progress = Math.min(progress, 100); // Garante que não ultrapasse 100%
-            progressValue.style.width = progress + "%";
-            setTimeout(simulateLoading, 100);
-        } else {
-            setTimeout(function () {
-                loadingScreen.style.opacity = '0';
-                loadingScreen.style.transition = 'opacity 0.5s ease';
-                setTimeout(function () {
-                    loadingScreen.style.display = 'none';
-                }, 500);
-            }, 500);
-        }
-    }
+  // Simula o carregamento
+  var percentageLoaded = 0;
+  var interval = setInterval(function() {
+      percentageLoaded++;
+      updateProgressBar(percentageLoaded);
+      if (percentageLoaded >= 100) {
+          clearInterval(interval);
+          // Após a página carregar completamente, esconde a tela de carregamento
+          document.getElementById('loadingScreen').style.display = 'none';
+      }
+  }, 350); // Ajuste este valor conforme necessário para simular a velocidade de carregamento
 
-    simulateLoading();
+  // Detecta quando a página completa o carregamento
+  window.onload = function() {
+      percentageLoaded = 100;
+      updateProgressBar(percentageLoaded);
+      setTimeout(function() { // Pequeno delay para garantir a visualização da barra cheia antes de desaparecer
+          document.getElementById('loadingScreen').style.display = 'none';
+      }, 1500); // Ajuste este delay conforme desejado
+  };
 });
